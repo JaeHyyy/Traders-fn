3031459c174420caa80a7205d35d6aea8a625013
yoon9i <todisto29@naver.com>
2024-07-19 10:09:43 +0900

    login 진행중

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider, Navigate, useRouteLoaderData } from "react-router-dom";
 import RootLayout from "./pages/Root";
+import Login from './pages/Login';
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
   {
     path: "/home",
     element: <RootLayout />,
---
+++ src/pages/Login.js
@@ -0,0 +1,16 @@
+import logo from '../assets/logo.png'
+import '../pages/Login.css'
+
+function Login() {
+    return (
+        <div className="login_page">
+            <>
+                <img src={logo} alt="로고" className="logo" />
+
+            </>
+
+        </div>
+    );
+}
+
+export default Login;
