ec6a7641375186d7cb593336c0559390e36796a4
yoon9i <todisto29@naver.com>
2024-07-19 13:18:32 +0900

    login page 수정중

---
+++ src/pages/Login.js
@@ -0,0 +1,24 @@
+import logo from '../assets/logo.png';
+import '../pages/Login.css';
+
+function Login() {
+    return (
+        <div id="login_page">
+            <img src={logo} alt="로고" className="logo" />
+            <form method="" action="" id="login-form">
+                <input type="text" name="id" placeholder="아이디" />
+                <input type="password" name="password" placeholder="Password" />
+                <div className="form-options">
+                    <label htmlFor="remember-check-id">
+                        <input type="checkbox" id="remember-check-id" />아이디 저장하기
+                    </label>
+                    <a href="" className="reset-password">비밀번호 재설정</a>
+                </div>
+                <input type="submit" value="로그인" className="btn-login" />
+                <input type="submit" value="회원가입" className="btn-signup" />
+            </form>
+        </div>
+    );
+}
+
+export default Login;
