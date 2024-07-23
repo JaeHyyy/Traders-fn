6c6dda4f1e92e195a9a17f69b4f64f1afb3dba67
yoon9i <todisto29@naver.com>
2024-07-24 00:27:52 +0900

    signup & login -> complete -> main

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,50 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import login from './Login.module.css';
 import WindowSize from '../components/WindowSize';
 
 function Login() {
+    const [credentials, setCredentials] = useState({ branchId: '', passwd: '' });
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8090/traders/login', credentials)
+            .then(response => {
+                console.log(response.data);
+                // 로그인 성공 후 메인 페이지로 이동
+                navigate('/');
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+                alert('로그인 실패: ' + error.message);
+            });
+    };
 
     return (
         <div id={login.login_page}>
             <div className={login.login_box}>
                 <img src={logo} alt="로고" className={login.logo} />
-                <form method="" action="" id={login.login_form}>
-                    <input type="text" name="id" placeholder="Email" className={login.input} defaultValue="" /><br />
-                    <input type="password" name="password" placeholder="Password" className={login.input} defaultValue="" /><br />
+                <form onSubmit={handleSubmit} id={login.login_form}>
+                    <input type="text" name="branchId" placeholder="Email" className={login.input} value={credentials.branchId} onChange={handleChange} /><br />
+                    <input type="password" name="passwd" placeholder="Password" className={login.input} value={credentials.passwd} onChange={handleChange} /><br />
                     <div className={login.form_options}>
                         <label htmlFor="remember-check-id">
                             <input type="checkbox" id="remember-check-id" />아이디 저장하기
                         </label><br />
                         <a href="" className={login.reset_password}>비밀번호 재설정</a>
                     </div><br />
-                    <input value="로그인" className={login.btn_login} /><br />
-                    <input value="회원가입" className={login.btn_signup} /><br />
+                    <input type="submit" value="로그인" className={login.btn_login} /><br />
+                    <input type="button" value="회원가입" className={login.btn_signup} onClick={() => navigate('/signup')} /><br />
                 </form>
             </div>
         </div>
@@ -26,4 +52,3 @@ function Login() {
 }
 
 export default Login;
-
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,136 +1,105 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png';
-import userIcon from '../assets/user.png'; // Import the user icon as an image
+import userIcon from '../assets/user.png';
 import lockIcon from '../assets/lock.png';
 import phoneIcon from '../assets/phone.png';
 import ofName from '../assets/officename.png';
 import ofPhone from '../assets/officephone.png';
-import ofDate from '../assets/calendar.png';
 import styles from "./Signup.module.css";
 
 const Signup = () => {
-    const [fileName, setFileName] = useState('사업자등록증등록');
+    const [formData, setFormData] = useState({
+        branchId: '',
+        passwd: '',
+        branchName: '',
+        nickname: '',
+        branchNum: '',
+        branchImage: '',
+        post: '',
+        addr1: '',
+        addr2: '',
+        phoneNum: ''
+    });
 
-    useEffect(() => {
-        window.daum = window.daum || {};
+    const navigate = useNavigate();
 
-        const script = document.createElement('script');
-        script.src = "http://dmaps.daum.net/map_js_init/postcode.v2.js";
-        script.async = true;
-        script.onload = () => {
-            window.daum = window.daum || {};
-        };
-        document.body.appendChild(script);
-
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
-
-    const handlePostcodeSearch = () => {
-        if (window.daum && window.daum.Postcode) {
-            new window.daum.Postcode({
-                oncomplete: function (data) {
-                    var fullRoadAddr = data.roadAddress;
-                    var extraRoadAddr = '';
-
-                    if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
-                        extraRoadAddr += data.bname;
-                    }
-                    if (data.buildingName !== '' && data.apartment === 'Y') {
-                        extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-                    }
-                    if (extraRoadAddr !== '') {
-                        extraRoadAddr = ' (' + extraRoadAddr + ')';
-                    }
-                    if (fullRoadAddr !== '') {
-                        fullRoadAddr += extraRoadAddr;
-                    }
-
-                    document.getElementById('postcode').value = data.zonecode;
-                    document.getElementById('roadAddress').value = fullRoadAddr;
-                    document.getElementById('jibunAddress').value = data.jibunAddress;
-
-                    if (data.autoRoadAddress) {
-                        var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
-                        document.getElementById('guide').innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
-                    } else if (data.autoJibunAddress) {
-                        var expJibunAddr = data.autoJibunAddress;
-                        document.getElementById('guide').innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
-                    } else {
-                        document.getElementById('guide').innerHTML = '';
-                    }
-                }
-            }).open();
-        } else {
-            console.error('Daum 우편번호 API가 로드되지 않았습니다.');
-        }
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
     };
 
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
-        setFileName(file ? file.name : '사업자등록증등록');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8090/traders/signup', formData)
+            .then(response => {
+                console.log(response.data);
+                alert('회원가입 성공');
+                navigate('/login'); // 회원가입 성공 시 로그인 페이지로 이동
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+                alert('회원가입 실패: ' + error.message);
+            });
     };
 
     return (
         <div id={styles.signup_page}>
             <div className={styles.signup_box}>
-                <form method="" action="" id={styles.signup_form}>
+                <form onSubmit={handleSubmit} id={styles.signup_form}>
                     <img src={logo} alt="로고" className={styles.logo} />
 
                     <div className={styles.inputContainer}>
                         <img src={userIcon} alt="user icon" className={styles.icon} />
-                        <input type="text" name="id" placeholder="@sample.com" className={styles.input} defaultValue="" />
+                        <input type="text" name="branchId" placeholder="@sample.com" className={styles.input} value={formData.branchId} onChange={handleChange} />
                     </div>
                     <br />
                     <div className={styles.inputContainer}>
                         <img src={lockIcon} alt="lock icon" className={styles.icon} />
-                        <input type="password" name="password" placeholder="Password" className={styles.input} defaultValue="" />
+                        <input type="password" name="passwd" placeholder="Password" className={styles.input} value={formData.passwd} onChange={handleChange} />
                     </div>
                     <br />
                     <div className={styles.inputContainer}>
                         <img src={phoneIcon} alt="phone icon" className={styles.icon} />
-                        <input type="tel" name="phonenumber" placeholder="'-' 없이 입력해주세요." className={styles.input} defaultValue="" />
+                        <input type="tel" name="phoneNum" placeholder="'-' 없이 입력해주세요." className={styles.input} value={formData.phoneNum} onChange={handleChange} />
                     </div>
                     <br />
                     <hr />
                     <div className={styles.addressSection}>
                         <div className={styles.inputContainer}>
-                            <input type="text" name="postcode" id="postcode" className={`${styles.input} ${styles.addressInput}`} placeholder="우편번호" />
+                            <input type="text" name="post" className={`${styles.input} ${styles.addressInput}`} placeholder="우편번호" value={formData.post} onChange={handleChange} />
                         </div>
                         <div>
-                            <button type="button" className={styles.button} onClick={handlePostcodeSearch}>우편번호 찾기</button>
+                            <button type="button" className={styles.button}>우편번호 찾기</button>
                         </div>
                     </div>
                     <div className={styles.addressSection}>
                         <div className={styles.inputContainer}>
-                            <input type="text" name="roadAddress" id="roadAddress" className={`${styles.input} ${styles.addressInput}`} placeholder="도로명주소" />
+                            <input type="text" name="addr1" className={`${styles.input} ${styles.addressInput}`} placeholder="도로명주소" value={formData.addr1} onChange={handleChange} />
                         </div>
                         <div className={styles.inputContainer}>
-                            <input type="text" name="jibunAddress" id="jibunAddress" className={`${styles.input} ${styles.addressInput}`} placeholder="지번주소" />
+                            <input type="text" name="addr2" className={`${styles.input} ${styles.addressInput}`} placeholder="지번주소" value={formData.addr2} onChange={handleChange} />
                             <span id="guide" className={styles.guide}></span>
                         </div>
                     </div>
                     <hr />
                     <div className={styles.upload}>
-                        <input type="text" className={styles.upload_name} value={fileName} placeholder="사업자등록증등록" readOnly />
+                        <input type="text" className={styles.upload_name} value={formData.branchImage} placeholder="사업자등록증등록" readOnly />
                         <label htmlFor="file" className={styles.upload_label}>파일찾기</label>
-                        <input type="file" id="file" onChange={handleFileChange} />
+                        <input type="file" id="file" onChange={(e) => setFormData({ ...formData, branchImage: e.target.files[0].name })} />
                     </div>
                     <br />
                     <div className={styles.inputContainer}>
                         <img src={ofName} alt="office name icon" className={styles.icon} />
-                        <input type="text" name="branchName" id="branchName" className={styles.input} placeholder="지점명" />
-                    </div>
-                    <br />
-                    <div className={styles.inputContainer}>
-                        <img src={ofDate} alt="office date icon" className={styles.icon} />
-                        <input type="text" name="issuedate" id="issuedate" className={styles.input} placeholder="발급일" />
+                        <input type="text" name="branchName" className={styles.input} placeholder="지점명" value={formData.branchName} onChange={handleChange} />
                     </div>
                     <br />
                     <div className={styles.inputContainer}>
                         <img src={ofPhone} alt="office phone number icon" className={styles.icon} />
-                        <input type="text" name="businessNumber" id="businessNumber" className={styles.input} placeholder="사업자번호" />
+                        <input type="text" name="branchNum" className={styles.input} placeholder="사업자번호" value={formData.branchNum} onChange={handleChange} />
                     </div>
                     <br />
                     <div className={styles.signup}>
