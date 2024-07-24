1fbf81b5a7d5c8862ad3e54fc66aac15858c92fb
ponion <qkrwndjs9898@gmail.com>
2024-07-24 17:13:35 +0900

    receipt css commit

--- src/pages/Receipt.js
+++ src/pages/Receipt.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import Table from '../components/Table';
+import Receiptt from './Receipt.module.css'
 
 const Receipt=() => {
   const [movement, setMovement] = useState([]);
-  const [selectedRows, setSelectedRows] = useState([]);
 
   const columns = [
     { header: '순번', accessor: null },
@@ -26,59 +26,7 @@ const Receipt=() => {
         });
     }, []);
 
-    const handleSelectAll = (event) => {
-      if (event.target.checked) {
-        setSelectedRows(movement.map((_, index) => index));
-      } else {
-        setSelectedRows([]);
-      }
-    };
-  
-    const handleSelectRow = (rowIndex) => {
-      if (selectedRows.includes(rowIndex)) {
-        setSelectedRows(selectedRows.filter(index => index !== rowIndex));
-      } else {
-        setSelectedRows([...selectedRows, rowIndex]);
-      }
-    };
-
-
-    return (
-      <table className="receipt-table">
-        <thead>
-          <tr>
-            <th></th>
-            <input
-              type="checkbox"
-              onChange={handleSelectAll}
-              checked={selectedRows.length === movement.length}
-            />
-            {columns.map((column, index) => (
-              <th key={index}>{column.header}</th>))}
-          </tr>
-        </thead>
-        <tbody>
-          {movement.map((row, rowIndex) =>(
-            <tr key={rowIndex}>
-              <td>
-                <input
-                type="checkbox"
-                onChange={() => handleSelectRow(rowIndex)}
-                checked={selectedRows.includes(rowIndex)}
-                />
-              </td>
-                {columns.map((column, colIndex) => (
-                  <td key={colIndex}>
-                    {column.render ? column.render(row) : row[column.accessor]}
-                      {/* render는 이미지나 입력필드 같은 특수한 데이터
-                 accessor는 데이터 키값 */}
-                  </td>
-                ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
+    return <Table columns={columns} data={movement} />;
 };
 
 export default Receipt;
