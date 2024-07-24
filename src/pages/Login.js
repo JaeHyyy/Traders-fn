5f72e83a088eaf2521b496c250b5bdc8644a36f6
yoon9i <todisto29@naver.com>
2024-07-24 09:40:28 +0900

    login&signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import login from './Login.module.css';
-import WindowSize from '../components/WindowSize';
 
 function Login() {
     const [credentials, setCredentials] = useState({ branchId: '', passwd: '' });
+    const [rememberMe, setRememberMe] = useState(false);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const savedBranchId = localStorage.getItem('branchId');
+        if (savedBranchId) {
+            setCredentials((prev) => ({ ...prev, branchId: savedBranchId }));
+            setRememberMe(true);
+        }
+    }, []);
 
     const handleChange = (e) => {
         setCredentials({
@@ -16,12 +24,20 @@ function Login() {
         });
     };
 
+    const handleRememberMeChange = (e) => {
+        setRememberMe(e.target.checked);
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
         axios.post('http://localhost:8090/traders/login', credentials)
             .then(response => {
                 console.log(response.data);
-                // 로그인 성공 후 메인 페이지로 이동
+                if (rememberMe) {
+                    localStorage.setItem('branchId', credentials.branchId);
+                } else {
+                    localStorage.removeItem('branchId');
+                }
                 navigate('/');
             })
             .catch(error => {
@@ -39,9 +55,9 @@ function Login() {
                     <input type="password" name="passwd" placeholder="Password" className={login.input} value={credentials.passwd} onChange={handleChange} /><br />
                     <div className={login.form_options}>
                         <label htmlFor="remember-check-id">
-                            <input type="checkbox" id="remember-check-id" />아이디 저장하기
+                            <input type="checkbox" id="remember-check-id" checked={rememberMe} onChange={handleRememberMeChange} />아이디 저장하기
                         </label><br />
-                        <a href="" className={login.reset_password}>비밀번호 재설정</a>
+                        <a href="/reset-password" className={login.reset_password}>비밀번호 재설정</a>
                     </div><br />
                     <input type="submit" value="로그인" className={login.btn_login} /><br />
                     <input type="button" value="회원가입" className={login.btn_signup} onClick={() => navigate('/signup')} /><br />
